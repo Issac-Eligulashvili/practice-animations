0c978a982bfd7c62c18ae6269b60ee16edccb57a
Issac-Eligulashvili <issac.eli2007@gmail.com>
2024-02-20 13:48:39 -0500

    scissors!

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 let playerFist = document.querySelector('.fist');
+let deltaTime = 100;
 
 function shake() {
-    const fistShakeAnim = setInterval(function() {
-     // Fist going down
-    playerFist.src = "/fist pump animations/1.png";
-    setTimeout(function () { playerFist.src = "/fist pump animations/2.png"; }, 160);
-    setTimeout(function () { playerFist.src = "/fist pump animations/3.png"; }, 320);
-    setTimeout(function () { playerFist.src = "/fist pump animations/4.png"; }, 480);
-    setTimeout(function () { playerFist.src = "/fist pump animations/5.png"; }, 640);
-    setTimeout(function () { playerFist.src = "/fist pump animations/7.png"; }, 960);
-
+    let fistShakeAnim = setInterval(function() {
      // Fist going up
-    setTimeout(function () { playerFist.src = "/fist pump animations/5.png"; }, 1120);
-    setTimeout(function () { playerFist.src = "/fist pump animations/4.png"; }, 1280);
-    setTimeout(function () { playerFist.src = "/fist pump animations/3.png"; }, 1440);
-    setTimeout(function () { playerFist.src = "/fist pump animations/2.png"; }, 1600);
-    setTimeout(function () { playerFist.src = "/fist pump animations/1.png"; }, 1760);
-    }, 1760)
+    playerFist.src = "/fist pump animations/6.png";
+    setTimeout(function () { playerFist.src = "/fist pump animations/5.png"; }, deltaTime);
+    setTimeout(function () { playerFist.src = "/fist pump animations/4.png"; }, deltaTime * 2);
+    setTimeout(function () { playerFist.src = "/fist pump animations/3.png"; }, deltaTime * 3);
+    setTimeout(function () { playerFist.src = "/fist pump animations/2.png"; }, deltaTime * 4);
+    setTimeout(function () { playerFist.src = "/fist pump animations/1.png"; }, deltaTime * 5);
+    // Fist going down
+    setTimeout(function () { playerFist.src = "/fist pump animations/1.png"; }, deltaTime * 6);
+    setTimeout(function () { playerFist.src = "/fist pump animations/2.png"; }, deltaTime * 7);
+    setTimeout(function () { playerFist.src = "/fist pump animations/3.png"; }, deltaTime * 7);
+    setTimeout(function () { playerFist.src = "/fist pump animations/4.png"; }, deltaTime * 8);
+    setTimeout(function () { playerFist.src = "/fist pump animations/5.png"; }, deltaTime * 9);
+    }, deltaTime * 11)
 
-    setTimeout(function() {clearInterval(fistShakeAnim)}, 5280)
+    setTimeout(function() {clearInterval(fistShakeAnim)}, deltaTime * 33);
+    
+    
+
+    setTimeout(function () { playerFist.src = "/fist pump animations/5.png"; }, deltaTime * 45);
+    setTimeout(function () { playerFist.src = "/fist pump animations/4.png"; }, deltaTime * 46);
+    setTimeout(function () { playerFist.src = "/fist pump animations/3.png"; }, deltaTime * 47);
+    setTimeout(function () { playerFist.src = "/fist pump animations/2.png"; }, deltaTime * 48);
+    setTimeout(function () { playerFist.src = "/fist pump animations/1.png"; }, deltaTime * 49);
+}
+
+function scissors() {
+    shake();
+    setTimeout(function () { playerFist.src = "/fist pump animations/scissors/scissors-1.png"; }, deltaTime * 50);
+    setTimeout(function () { playerFist.src = "/fist pump animations/scissors/scissors-2.png"; }, deltaTime * 51);
+    setTimeout(function () { playerFist.src = "/fist pump animations/scissors/scissors-3.png"; }, deltaTime * 52);
+    setTimeout(function () { playerFist.src = "/fist pump animations/scissors/scissors-4.png"; }, deltaTime * 53);
+    setTimeout(function () { playerFist.src = "/fist pump animations/scissors/scissors-5.png"; }, deltaTime * 54);
+}
+function rock() {
+    shake();
+    setTimeout(function () { playerFist.src = "/fist pump animations/1.png"; }, deltaTime * 50);
+    setTimeout(function () { playerFist.src = "/fist pump animations/2.png"; }, deltaTime * 51);
+    setTimeout(function () { playerFist.src = "/fist pump animations/3.png"; }, deltaTime * 52);
+    setTimeout(function () { playerFist.src = "/fist pump animations/4.png"; }, deltaTime * 53);
+    setTimeout(function () { playerFist.src = "/fist pump animations/5.png"; }, deltaTime * 54);
 }
 
